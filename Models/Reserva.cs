3cabe3efcdcce81dc2f545f45841ac8378a18ac4
diego.exe <diwalker13@icloud.com>
2023-09-07 01:12:04 -0300

    Desafio DIO
    Complementação do codigo para a atividade do bootcamp .NET Developer

---
+++ Models/Reserva.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verifica se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (Hospedes == null)
+            {
+                Hospedes = hospedes;
+            }
+            else if (Hospedes.Count + hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes.AddRange(hospedes);
+            }
+            else
+            {
+                // Retorna uma exceção caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Capacidade excedida. Não é possível cadastrar mais hóspedes.");
+            }
+        }
+
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 0.1m; // Aplicando desconto de 10%
+            }
+
+            return valor;
+        }
+    }
+}
